0f53611628d67d2dabce00ba2727b053c54fd20d
testfooyy <dicydia@gmail.com>
2017-07-24 18:18:49 +0900

    Initial Commit

---
+++ CellTap/CustomTableViewCell.swift
@@ -0,0 +1,52 @@
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var testImageView: UIImageView!
+    @IBOutlet weak var testLabel: UILabel!
+    
+    @IBOutlet weak var fooImageView: UIImageView!
+    @IBOutlet weak var fooLabel: UILabel!
+    
+    
+    
+    func testCell(test: String, list: String) {
+        testLabel.text = test
+        let session = URLSession(configuration: URLSessionConfiguration.default, delegate: nil, delegateQueue: OperationQueue.main)
+        let req = NSURLRequest(url: NSURL(string: list)! as URL)
+        let task = session.dataTask(with: req as URLRequest, completionHandler: { (data, response, error) in
+            if let imageData = data , let image = UIImage(data: imageData) {
+                self.testImageView.image = image
+                
+            } else {
+                print("Error!")
+            }
+        })
+        task.resume()
+        
+        
+    }
+    
+    func fooCell(test: String, list: String) {
+        fooLabel.text = test
+        let session = URLSession(configuration: URLSessionConfiguration.default, delegate: nil, delegateQueue: OperationQueue.main)
+        let req = NSURLRequest(url: NSURL(string: list)! as URL)
+        let task = session.dataTask(with: req as URLRequest, completionHandler: { (data, response, error) in
+            if let imageData = data , let image = UIImage(data: imageData) {
+                self.fooImageView.image = image
+            } else {
+                print("Error!")
+            }
+        })
+        task.resume()
+    }
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+}
---
+++ CellTap/TableViewController.swift
@@ -0,0 +1,50 @@
+import UIKit
+
+class TableViewController: UITableViewController {
+    var myItems = ["TEST1", "TEST2", "TEST3"]
+    var list = ["https://pbs.twimg.com/media/DFVMcBQUIAAVymK.jpg", "https://pbs.twimg.com/media/DFYrlUeVoAAwLLx.jpg", "https://pbs.twimg.com/media/DFZCE9-XoAITns1.jpg"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.estimatedRowHeight = 20
+        tableView.rowHeight = UITableViewAutomaticDimension
+
+    }
+ 
+    
+    @IBAction func foo(_ sender: UITapGestureRecognizer) {
+        let tappedLocation = sender.location(in: tableView)
+        let tappedIndexPath = tableView.indexPathForRow(at: tappedLocation)
+        let tappedRow = tappedIndexPath?.row
+        print(tappedRow)
+    }
+    
+    
+    @IBAction func bar(_ sender: UITapGestureRecognizer) {
+            let tappedLocation = sender.location(in: tableView)
+            let tappedIndexPath = tableView.indexPathForRow(at: tappedLocation)
+            let tappedRow = tappedIndexPath?.row
+            print(tappedRow)
+    }
+    
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        if self.myItems[indexPath.row] == "TEST1" {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "testCell") as! CustomTableViewCell
+            
+            cell.testCell(test: myItems[indexPath.row] , list: list[indexPath.row] )
+            cell.layoutIfNeeded()
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "fooCell") as! CustomTableViewCell
+            cell.fooCell(test: myItems[indexPath.row] , list: list[indexPath.row] )
+            cell.layoutIfNeeded()
+            return cell
+        }
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myItems.count
+    }
+    
+}
